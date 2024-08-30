80d15c03512e2523af12fd510b76aed5da8fd1f3
maged gamil <magedgamil200@gmail.com>
2024-08-30 17:37:29 +0300

    Add project files.

---
+++ CustomLINQMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using Shared;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace CustomLINQMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.Employees;
+        
+            var page1 = employees.paginate(1, 10);
+            var page2 = employees.paginate(2, 10);
+            var page3 = employees.paginate(3, 10);
+
+            page1.Print("Page 1");             
+            page2.Print("Page 2");             
+            page3.Print("Page 3");             
+         
+
+           var page1Filtered = employees.WhereWithPaginate(e => e.Salary > 1000, 1, 10);
+            page1Filtered.Print("Page 1 Filtered");
+            Console.ReadKey();
+        }
+         
+    }
+}
---
+++ Shared/Extensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shared;
+
+public static class Extensions
+{
+    private static Random random = new Random();
+    public static void Print<T>(this IEnumerable<T> source, string title)
+    {
+        if (source == null)
+            return;
+        Console.WriteLine();
+        Console.WriteLine("┌───────────────────────────────────────────────────────┐");
+        Console.WriteLine($"│   {title.PadRight(52, ' ')}│");
+        Console.WriteLine("└───────────────────────────────────────────────────────┘");
+        Console.WriteLine();
+        foreach (var item in source)
+        {
+            if (typeof(T).IsValueType)
+                Console.Write($" {item} "); // 1, 2, 3
+            else
+                Console.WriteLine(item);
+        }
+    }
+
+    public static IEnumerable<T> paginate<T>(this IEnumerable<T> source, int page = 1, int pageSize = 10)
+    {
+        if (source == null)
+            throw new ArgumentNullException($"{nameof(source)}");
+
+        if (page <= 0)
+        {
+            page = 1;
+            throw new ArgumentException($"{nameof(page)} must be greater than 0");
+        }
+
+        if (pageSize <= 0)
+        {
+            pageSize = 10;
+            throw new ArgumentException($"{nameof(pageSize)} must be greater than 0");
+        }
+
+        if (!source.Any())
+            return [];
+
+        return source.Skip((page - 1) * pageSize).Take(pageSize);
+
+    }
+    public static IEnumerable<T> WhereWithPaginate<T>(this IEnumerable<T> source, Func<T,bool>predicate, int page = 1, int pageSize = 10)
+    {
+        if (source == null)
+            throw new ArgumentNullException($"{nameof(source)}");
+
+       var filtered = Enumerable.Where(source, predicate);
+
+        return paginate(filtered, page, pageSize);
+
+    }
+
+}
